f063d8d52b2a87cc167da26b0fd9f28e43b5358c
Mel Rumsey <melsrumsey@gmail.com>
2020-11-17 12:54:16 -0600

    First push

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizonalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        rigidBody.velocity = new Vector2(horizontalInput, verticalInput); // Vector 2 takes 2 values, the x and the y
+
+        
+    }
+}
