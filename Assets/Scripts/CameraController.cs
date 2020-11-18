2241afb377e01be8308c036e25704290bb6eb88a
Mel Rumsey <melsrumsey@gmail.com>
2020-11-18 17:19:59 -0600

    save

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLoader : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    public GameObject player;
-
     // Start is called before the first frame update
     void Start()
     {
-        if (PlayerController.instance == null)
-        {
-            Instantiate(player);
-        }
+        
     }
 
     // Update is called once per frame
