b5130b845e0eb8db79fdfc0c41cfe7f6d786f247
Mel Rumsey <melsrumsey@gmail.com>
2021-02-02 18:47:11 -0600

    dialog and name changes

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -12,11 +12,15 @@ public class DialogManager : MonoBehaviour {
 
     public string[] dialogLines;
     public int currentLine;
+    private bool textStarted;
+
+    public static DialogManager instance;
 
     // Start is called before the first frame update
     void Start () {
+        instance = this;
 
-        dialogText.text = dialogLines[currentLine];
+        // dialogText.text = dialogLines[currentLine]; for testing
     }
 
     // Update is called once per frame
@@ -24,10 +28,47 @@ public class DialogManager : MonoBehaviour {
 
         if (dialogBox.activeInHierarchy) {
             if (Input.GetButtonUp ("Fire1")) {
-                currentLine++;
+                if (!textStarted) {
+                    currentLine++;
 
-                dialogText.text = dialogLines[currentLine];
+                    if (currentLine >= dialogLines.Length) {
+
+                        dialogBox.SetActive (false);
+                        PlayerController.instance.canMove = true;
+
+                    } else {
+                        CheckIfName ();
+
+                        dialogText.text = dialogLines[currentLine];
+
+                    }
+                } else {
+                    textStarted = false;
+                }
             }
         }
     }
+
+    public void ShowDialog (string[] newLines) {
+        dialogLines = newLines;
+
+        currentLine = 0;
+
+        CheckIfName ();
+
+        dialogText.text = dialogLines[currentLine];
+        dialogBox.SetActive (true);
+
+        textStarted = true;
+
+        PlayerController.instance.canMove = false;
+    }
+
+    public void CheckIfName () {
+        if (dialogLines[currentLine].StartsWith ("n-")) {
+            nameText.text = dialogLines[currentLine].Replace ("n-", "");
+            currentLine++;
+        }
+    }
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,58 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     public Rigidbody2D rigidBody;
     public float moveSpeed;
     public Animator myAnim;
 
-    public static PlayerController instance;  // reference to the script on Player-- only 1 allowed!
+    public static PlayerController instance; // reference to the script on Player-- only 1 allowed!
 
     public string areaTransitionName;
 
     private Vector3 bottomLeft;
     private Vector3 topRight;
 
+    public bool canMove = true;
 
-    void Start()
-    {
+    void Start () {
         // prevents character duplication
-        if(instance == null)
-        {
-            instance = this;  // instance is THIS script
-        }
-        else
-        {
-            if(instance != this)
-            {
-                Destroy(gameObject);
+        if (instance == null) {
+            instance = this; // instance is THIS script
+        } else {
+            if (instance != this) {
+                Destroy (gameObject);
             }
         }
-        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad (gameObject);
     }
 
+    void Update () {
+        if (canMove) {
+            rigidBody.velocity = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical")) * moveSpeed; // Vector 2 takes 2 values, the x and the y
 
-    void Update()
-    {
-        rigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed; // Vector 2 takes 2 values, the x and the y
+            myAnim.SetFloat ("moveX", rigidBody.velocity.x);
+            myAnim.SetFloat ("moveY", rigidBody.velocity.y);
 
-        myAnim.SetFloat("moveX", rigidBody.velocity.x);
-        myAnim.SetFloat("moveY", rigidBody.velocity.y);
-
-        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 ||  Input.GetAxisRaw("Vertical") == -1)
-        {
-            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
-            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+            if (Input.GetAxisRaw ("Horizontal") == 1 || Input.GetAxisRaw ("Horizontal") == -1 || Input.GetAxisRaw ("Vertical") == 1 || Input.GetAxisRaw ("Vertical") == -1) {
+                if (canMove) {
+                    myAnim.SetFloat ("lastMoveX", Input.GetAxisRaw ("Horizontal"));
+                    myAnim.SetFloat ("lastMoveY", Input.GetAxisRaw ("Vertical"));
+                }
+            }
+        } else {
+            rigidBody.velocity = Vector2.zero;
         }
 
-    transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeft.x, topRight.x), Mathf.Clamp(transform.position.y, bottomLeft.y, topRight.y), transform.position.z);
-
+        transform.position = new Vector3 (Mathf.Clamp (transform.position.x, bottomLeft.x, topRight.x), Mathf.Clamp (transform.position.y, bottomLeft.y, topRight.y), transform.position.z);
     }
 
-    public void SetBounds(Vector3 botLeftLimit, Vector3 topRightLimit)
-    {
-        bottomLeft = botLeftLimit + new Vector3(.5f, 1f, 0f);
-        topRight = topRightLimit+ new Vector3(-.5f, -1f, 0f);
+    public void SetBounds (Vector3 botLeftLimit, Vector3 topRightLimit) {
+        bottomLeft = botLeftLimit + new Vector3 (.5f, 1f, 0f);
+        topRight = topRightLimit + new Vector3 (-.5f, -1f, 0f);
     }
 }
